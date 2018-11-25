9fdaf46104df2f33584cf4f117a86ef922470733
Andy Phillips <andyphillips82@gmail.com>
2018-11-24 19:25:02 -0500

    db change

--- wp-config.php
+++ wp-config.php
@@ -20,16 +20,16 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'whhrescue');
+define('DB_NAME', 'db762979743');
 
 /** MySQL database username */
-define('DB_USER', 'root');
+define('DB_USER', 'dbo762979743');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'root');
+define('DB_PASSWORD', '456cbD89?!?');
 
 /** MySQL hostname */
-define('DB_HOST', 'localhost');
+define('DB_HOST', 'db762979743.hosting-data.io');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8mb4');
